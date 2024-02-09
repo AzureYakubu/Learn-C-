4c0fd4063d2b5caba3adfc572f104b82c3e84c7c
YakubuAzure <obsmediaofficial@gmail.com>
2024-02-09 09:27:45 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>(){
+                {"cheese", 5.99m},
+                {"carrots", 2.99m}
+            };
+
+            
+            
+            //Write You Code Above This Line
+            Console.WriteLine(itemPrices["cheese"].GetType());
+            Console.WriteLine(itemPrices["cheese"]);
+            Console.WriteLine(itemPrices["carrots"]);
+        }
+    }
+}
