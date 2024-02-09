e449c3634a46f0cbdf43a7288d19f46e95489f14
YakubuAzure <obsmediaofficial@gmail.com>
2024-02-09 09:33:05 +0000

    removed whitespace

--- Program.cs
+++ Program.cs
@@ -5,14 +5,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>(){
+            Dictionary<string, decimal> itemPrices = new(){
                 {"cheese", 5.99m},
                 {"carrots", 2.99m}
             };
 
-            
-            
-            //Write You Code Above This Line
             Console.WriteLine(itemPrices["cheese"].GetType());
             Console.WriteLine(itemPrices["cheese"]);
             Console.WriteLine(itemPrices["carrots"]);
