b8c10ea97e53a03be3db2255ed70c6de068f7463
YakubuAzure <obsmediaofficial@gmail.com>
2024-02-13 08:25:38 +0000

    added GetSum method

--- Program.cs
+++ Program.cs
@@ -3,8 +3,11 @@ namespace HelloWorld
 {
     internal class Program
     {
+        private static int totalValue;
+
         static void Main(string[] args)
         {
+            /*
             Dictionary<string, decimal> itemPrices = new(){
                 {"cheese", 5.99m},
                 {"carrots", 2.99m}
@@ -13,6 +16,27 @@ namespace HelloWorld
             Console.WriteLine(itemPrices["cheese"].GetType());
             Console.WriteLine(itemPrices["cheese"]);
             Console.WriteLine(itemPrices["carrots"]);
+            */
+
+            int[] ArrayToSum = [24, 100, 100, 50, 40, 65];
+
+            totalValue = GetSum(ArrayToSum);
+
+            Console.WriteLine(totalValue);
+        }
+
+        static private int GetSum(int[] ArrayToSum)
+        {
+            int totalValue = 0;
+            foreach(int number in ArrayToSum)
+            {
+                totalValue += number;
+            }
+            if(totalValue > 300)
+            {
+                return totalValue;
+            }
+            return 0;
         }
     }
 }
