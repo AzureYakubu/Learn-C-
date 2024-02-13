3c6088320325031dc12b82657266b12b8c17d6bb
YakubuAzure <obsmediaofficial@gmail.com>
2024-02-13 08:45:39 +0000

    printed out odd numbers

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 namespace HelloWorld
 {
     internal class Program
-    {
+    { /*
         private static int totalValue;
 
         static void Main(string[] args)
@@ -16,7 +16,7 @@ namespace HelloWorld
             Console.WriteLine(itemPrices["cheese"].GetType());
             Console.WriteLine(itemPrices["cheese"]);
             Console.WriteLine(itemPrices["carrots"]);
-            */
+            
 
             int[] ArrayToSum = [24, 100, 100, 50, 40, 65];
 
@@ -37,6 +37,33 @@ namespace HelloWorld
                 return totalValue;
             }
             return 0;
+        } */
+
+        public class Exercise
+    {
+        static void Main(string[] args)
+        {
+            List<int> myNumberList = new List<int>(){
+                2, 3, 5, 6, 7, 9, 10, 123, 324, 54
+            };
+            
+            foreach (int number in myNumberList)
+            {
+                    PrintIfOdd(number);
+            }
+
+            //Console.WriteLine(number);
         }
+        //Write Your Code Here
+        public static int PrintIfOdd(int oddNumber)
+        {
+            if(oddNumber % 2 == 1)
+                {
+                    Console.WriteLine(oddNumber);
+                }
+                return 0;
+        }
+        
     }
+}
 }
